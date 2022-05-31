539da952f7a4164ac19eecdf3ff71bc33408ecaf
Nicholas Weiss <njweiss4@gmail.com>
2022-05-31 15:01:32 -0400

    Added gameRound function to RockPaperScissors.js

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,5 +1,14 @@
 function computerPlay() {
-    const actions = ["Rock", "Paper", "Scissors"];
+    const actions = ["ROCK", "PAPER", "SCISSORS"];
     return actions[Math.floor(3*Math.random())];
+}
+
+function gameRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase()
+    if (playerSelection === computerSelection) 
+        return "It's a tie! You both chose " + computerSelection + "!";
+    if ((playerSelection == "ROCK" && computerSelection == "SCISSORS") || (playerSelection == "PAPER" && computerSelection == "ROCK") || (playerSelection == "SCISSORS" && computerSelection == "PAPER"))
+        return "You win! " + playerSelection + " beats " + computerSelection + "!";
+    return "You lose! " + computerSelection + " beats " + playerSelection +"!";
 }
 
