be04acf3dcdc34d920ca5057c0465809cad1da21
Nicholas Weiss <njweiss4@gmail.com>
2022-06-04 11:41:22 -0400

    Added DOM

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,3 +1,6 @@
+var humanScore = 0
+var computerScore = 0
+
 function computerPlay() {
     const actions = ["ROCK", "PAPER", "SCISSORS"];
     return actions[Math.floor(3*Math.random())];
@@ -7,20 +10,36 @@ function gameRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toUpperCase()
     if (playerSelection === computerSelection) 
         return "It's a tie! You both chose " + computerSelection + "!";
-    if ((playerSelection == "ROCK" && computerSelection == "SCISSORS") || (playerSelection == "PAPER" && computerSelection == "ROCK") || (playerSelection == "SCISSORS" && computerSelection == "PAPER"))
+    if ((playerSelection == "ROCK" && computerSelection == "SCISSORS") || (playerSelection == "PAPER" && computerSelection == "ROCK") || (playerSelection == "SCISSORS" && computerSelection == "PAPER")){
+        humanScore++
         return "You win! " + playerSelection + " beats " + computerSelection + "!";
+    }
+    computerScore++
     return "You lose! " + computerSelection + " beats " + playerSelection +"!";
 }
 
-function game() {
-    let playerSelection;
-    alert("Let's play rock paper scissors!")
-    for (let i = 0; i < 5; i++) {
-        playerSelection = prompt("Choose your weapon!")
-        alert(gameRound(playerSelection, computerPlay()))
-        
-    }
-}
+function game(){
+    const buttons = document.querySelectorAll("button")
 
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            let gameResult = gameRound(button.textContent, computerPlay())
+            resultSection = document.querySelector(".results")
+            resultSection.textContent = gameResult
+            computerScoreSection = document.querySelector(".computerScore")
+            humanScoreSection = document.querySelector(".humanScore")
+            computerScoreSection.textContent = `The computer's Score: ${computerScore}`
+            humanScoreSection.textContent = `Your Score: ${humanScore}`
+            if (humanScore >= 5) {
+                document.querySelector("#container").remove()
+                setTimeout(()=>{alert('You won!')}, 5)
+            }
+            if (computerScore >= 5) {
+                document.querySelector("#container").remove()
+                setTimeout(()=>{alert('You lost!')}, 5)
+            }
+        })
+    })
+}
 
 game()
